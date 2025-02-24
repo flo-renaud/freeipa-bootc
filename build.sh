f8aa4f478b48f13d85a37408be8531d00aff598c
Florence Blanc-Renaud <flo@redhat.com>
2025-02-24 17:10:54 +0100

    Add other client packages
    Add freeipa-client-samba
        freeipa-client-epn
        freeipa-client-encrypted-dns
    
    Signed-off-by: Florence Blanc-Renaud <flo@redhat.com>

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set -ouex pipefail
 
 dnf5 install -y 'dnf5-command(copr)'
 dnf copr enable -y frenaud/freeipa-bootc
-dnf install -y freeipa-client 
+dnf install -y freeipa-client freeipa-client-epn freeipa-client-encrypted-dns freeipa-client-samba
 dnf copr disable -y frenaud/freeipa-bootc
 
